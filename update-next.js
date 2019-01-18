874750a7f2192d590485807e6112bd99cb796a39
王文璐 <kingwenlu@gmail.com>
2019-01-18 16:01:18 +0800

    fix publish next

---
+++ update-next.js
@@ -0,0 +1,9 @@
+const childProcess = require('child_process')
+const fs = require('fs')
+
+const commit = childProcess.execSync('git rev-parse HEAD').toString().slice(0, 8)
+const package = JSON.parse(fs.readFileSync('./package.json').toString())
+
+package.version = `${package.version}-${commit}`
+
+fs.writeFileSync('./package.json', JSON.stringify(package, undefined, 2))
