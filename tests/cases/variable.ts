574b4ecefc6b8ca5f19181f6b63e3fccc24bd139
AngryPowman <138001655@qq.com>
2019-02-13 15:08:02 +0800

    fix filename typo

---
+++ tests/cases/variable.ts
@@ -0,0 +1,22 @@
+var a = 1
+let b = 1
+const c = 1
+var d = 1,
+  d1 = 2
+let e = 1,
+  e1 = 2
+const f = 1,
+  f1 = 2
+const g = (e = b = 1)
+const h = ''
+const i = true
+const o = { name: 'kingwl' }
+var x = (function(number: number) {
+  return number * number
+})(4)
+var xx = function() {
+  return function() {}
+}
+export const j = 1
+declare const k: number
+a++
