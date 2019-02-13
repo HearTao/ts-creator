36b102189ff28231570d141b8293dd8b7f13fed6
AngryPowman <138001655@qq.com>
2019-02-13 15:11:47 +0800

    prettier

--- tests/cases/generics.ts
+++ tests/cases/generics.ts
@@ -1,90 +1,90 @@
 function generics<T>(arg: T): T {
-  return arg;
+  return arg
 }
-generics(123);
-generics<any[]>([]);
-generics<string>("Hello");
+generics(123)
+generics<any[]>([])
+generics<string>('Hello')
 
 function generics2<T>(arg: T[]): T[] {
-  return arg;
+  return arg
 }
-generics2<number>([123, 456]);
+generics2<number>([123, 456])
 
 function generics3<T>(arg: T): T {
-  return arg;
+  return arg
 }
-let gv: <U>(arg: U) => U = generics3;
+let gv: <U>(arg: U) => U = generics3
 
 interface GenericIdentityFn<T> {
-  (arg: T): T;
+  (arg: T): T
 }
 
 function generics4<T>(arg: T): T {
-  return arg;
+  return arg
 }
-let gvv: GenericIdentityFn<number> = generics4;
+let gvv: GenericIdentityFn<number> = generics4
 
 class GenericNumber<T> {
-  zeroValue!: T;
-  add!: (x: T, y: T) => T;
+  zeroValue!: T
+  add!: (x: T, y: T) => T
 }
 
-let myGenericNumber = new GenericNumber<number>();
-myGenericNumber.zeroValue = 0;
+let myGenericNumber = new GenericNumber<number>()
+myGenericNumber.zeroValue = 0
 myGenericNumber.add = function(x, y) {
-  return x + y;
-};
+  return x + y
+}
 
 function getProperty<T, K extends keyof T>(obj: T, key: K) {
-  return obj[key];
+  return obj[key]
 }
 
-let x = { a: 1, b: 2, c: 3, d: 4 };
-getProperty(x, "a");
+let x = { a: 1, b: 2, c: 3, d: 4 }
+getProperty(x, 'a')
 
 interface Lengthwise {
-  length: number;
+  length: number
 }
 
 function loggingIdentity<T extends Lengthwise>(arg: T): T {
-  return arg;
+  return arg
 }
-loggingIdentity({ length: 10, value: 3 });
+loggingIdentity({ length: 10, value: 3 })
 
 function getProperty2<T, K extends keyof T>(obj: T, key: K) {
-  return obj[key];
+  return obj[key]
 }
 
-let x2 = { a: 1, b: 2, c: 3, d: 4 };
-getProperty(x2, "a");
+let x2 = { a: 1, b: 2, c: 3, d: 4 }
+getProperty(x2, 'a')
 
 function create<T>(c: { new (): T }): T {
-  return new c();
+  return new c()
 }
 
 class BeeKeeper {
-  hasMask!: boolean;
+  hasMask!: boolean
 }
 
 class ZooKeeper {
-  nametag!: string;
+  nametag!: string
 }
 
 class Animal {
-  numLegs!: number;
+  numLegs!: number
 }
 
 class Bee extends Animal {
-  keeper!: BeeKeeper;
+  keeper!: BeeKeeper
 }
 
 class Lion extends Animal {
-  keeper!: ZooKeeper;
+  keeper!: ZooKeeper
 }
 
 function createInstance<A extends Animal>(c: new () => A): A {
-  return new c();
+  return new c()
 }
 
-createInstance(Lion).keeper.nametag;
-createInstance(Bee).keeper.hasMask;
+createInstance(Lion).keeper.nametag
+createInstance(Bee).keeper.hasMask
