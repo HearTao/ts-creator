9449d70bf8a8f1c19e9861801f8544ad4697a8a7
王文璐 <kingwenlu@gmail.com>
2019-03-13 15:36:51 +0800

    support tsx and add test cases

--- tests/cases/tsx.tsx
+++ tests/cases/tsx.tsx
@@ -1,4 +1,5 @@
 declare const Comp: any
+declare const Comp1: any
 
 declare const props: any
 
@@ -7,3 +8,17 @@ const t1 = <Comp a={123} b="b" c />
 const t2 = <Comp />
 
 const t3 = <Comp {...props} />
+
+const t4 = <Comp>text</Comp>
+
+const t5 = (
+  <Comp>
+    <Comp1 />
+  </Comp>
+)
+
+const t6 = (
+  <>
+    <Comp />
+  </>
+)
